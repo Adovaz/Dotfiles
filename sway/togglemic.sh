a02c57b9f8543abee625800c3a9e9ef08b65390a
Avery Zavoda <avery.zavoda@gmail.com>
2025-11-05 17:11:14 +1300

    ⚙️ SwayOSD config

--- sway/togglemic.sh
+++ sway/togglemic.sh
@@ -3,10 +3,10 @@
 #toggle mic and mic light
 
 pactl set-source-mute \@DEFAULT_SOURCE@ toggle
-level=$(light -s sysfs/leds/platform::micmute -G)
-echo $level
-if [ "$level" = "0.00" ]; then
-	light -s sysfs/leds/platform::micmute -S 100.00   
-else
+state=$(pactl get-source-mute @DEFAULT_SOURCE@)
+if [ "$state" = "Mute: no" ]; then
 	light -s sysfs/leds/platform::micmute -S 0.00
+
+else
+	light -s sysfs/leds/platform::micmute -S 100.00   
 fi
