22beae03c507ff0c94fed0c2d73d114178969de5
Avery Zavoda <avery.zavoda@gmail.com>
2026-05-09 12:04:45 +1200

    Add Hyprland config

--- dotfiles/config/sway/scripts/togglemic.sh
+++ dotfiles/config/sway/scripts/togglemic.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-
 #toggle mic and mic light
 swayosd-client --input-volume mute-toggle
 state=$(pactl get-source-mute @DEFAULT_SOURCE@)
 if [ "$state" = "Mute: no" ]; then
 	light -s sysfs/leds/platform::micmute -S 0.00
-
 else
 	light -s sysfs/leds/platform::micmute -S 100.00   
 fi
